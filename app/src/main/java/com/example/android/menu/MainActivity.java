efbaa2d5e52ef8cbc0b83ab55d462a583e43d4c8
Elaine Thomas <el41net@gmail.com>
2018-02-10 19:28:31 -0700

    practice writing to log

--- app/src/main/java/com/example/android/menu/MainActivity.java
+++ app/src/main/java/com/example/android/menu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
 
+        TextView textViewLog1 = (TextView)findViewById(R.id.menu_item_1);
+        Log.i("MyActivity", textViewLog1.getText().toString());
+
         // Find second menu item TextView and print the text to the logs
+        TextView textViewLog2 = (TextView)findViewById(R.id.menu_item_2);
+        Log.i("MyActivity", textViewLog2.getText().toString());
 
         // Find third menu item TextView and print the text to the logs
+        TextView textViewLog3 = (TextView)findViewById(R.id.menu_item_3);
+        Log.i("MyActivity", textViewLog3.getText().toString());
 
     }
 }
